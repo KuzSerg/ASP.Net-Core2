ff927950cc37c247a524fb472ac41c03b8c6be99
Sergey <43466490+KuzSerg@users.noreply.github.com>
2019-02-14 00:00:05 +0300

    Updates

--- WebValidation/Controllers/AuthController.cs
+++ WebValidation/Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -13,6 +14,7 @@ using WebValidation.Model;
 
 namespace WebValidation.Controllers
 {
+    
     [Route("api/[controller]")]
     public class AuthController : Controller
     {
@@ -37,16 +39,21 @@ namespace WebValidation.Controllers
                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
                 };
 
-                var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecureKey"));
+                var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySuperSecureKey"));
 
                 var token = new JwtSecurityToken(
-                    issuer: "http://oec.com",
-                    audience: "http://oec.com",
-                    expires: DateTime.UtcNow.AddHours(1),
+                    issuer: "",
+                    audience: "",
+                    expires: DateTime.Now.AddHours(1),
                     claims: claims,
                     signingCredentials: new Microsoft.IdentityModel.Tokens.SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
                     );
-
+                var t = new JwtSecurityTokenHandler().WriteToken(token);
+                return Ok(new
+                {
+                    token = t,
+                    expiration = token.ValidTo
+                });
             }
             return Unauthorized();
         }
